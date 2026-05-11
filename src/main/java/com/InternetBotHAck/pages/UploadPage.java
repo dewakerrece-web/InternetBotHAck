284b4edc219f87573aeca9c154f6ee6518315425
dewakerrece-web <dewaker.r.ece@gmail.com>
2026-05-11 15:22:48 +0530

    the uploading page's class file has been created

---
+++ src/main/java/com/InternetBotHAck/pages/UploadPage.java
@@ -0,0 +1,27 @@
+package com.InternetBotHAck.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class UploadPage {
+    WebDriver driver;
+    By uploadInput=By.id("file-upload");
+    By uploadButton=By.id("file-submit");
+    By uploadedFileName=By.id("uploaded-files");
+    By pageTitle=By.tagName("h3");
+
+    public UploadPage(WebDriver driver){
+        this.driver=driver;
+    }
+    public void uploadFile(String filepath){
+        driver.findElement(uploadInput).sendKeys(filepath);
+        driver.findElement(uploadButton).click();
+    }
+    public String getUploadedFileName(){
+        return driver.findElement(uploadedFileName).getText();
+    }
+    public String getPageTitle(){
+        return driver.findElement(pageTitle).getText();
+    }
+
+}
