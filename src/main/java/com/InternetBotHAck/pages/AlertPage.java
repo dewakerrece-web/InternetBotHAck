ed87eb795f9575906c10c608d46996ce431e7910
dewakerrece-web <dewaker.r.ece@gmail.com>
2026-05-11 14:45:49 +0530

    The basic Structure of the Projects has been initialized

--- src/main/java/com/InternetBotHAck/pages/AlertPage.java
+++ src/main/java/com/InternetBotHAck/pages/AlertPage.java
@@ -1,5 +1,18 @@
 package com.InternetBotHAck.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class AlertPage {
+    WebDriver driver;
+
+    By jsAlertButton= By.xpath("//button[text()='Click for JS Alert']");
+    By jsConfirmButton=By.xpath("//button[text()='Click for JS Confirm']");
+    By jsPromptButton=By.xpath("//button[text()='Click for JS Prompt']");
+    By resultText=By.id("result");
+
+    public AlertPage(WebDriver driver){
+        this.driver=driver;
+    }
 
 }
