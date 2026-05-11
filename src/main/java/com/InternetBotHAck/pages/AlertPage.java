557525a149cfdcd56b14a779b9e6d99c169650c7
dewakerrece-web <dewaker.r.ece@gmail.com>
2026-05-11 14:56:54 +0530

    the third page class CheckboxPage has been initialized

--- src/main/java/com/InternetBotHAck/pages/AlertPage.java
+++ src/main/java/com/InternetBotHAck/pages/AlertPage.java
@@ -1,18 +1,33 @@
 package com.InternetBotHAck.pages;
-
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-
 public class AlertPage {
     WebDriver driver;
-
-    By jsAlertButton= By.xpath("//button[text()='Click for JS Alert']");
+    By jsAlertButton=By.xpath("//button[text()='Click for JS Alert']");
     By jsConfirmButton=By.xpath("//button[text()='Click for JS Confirm']");
     By jsPromptButton=By.xpath("//button[text()='Click for JS Prompt']");
     By resultText=By.id("result");
-
-    public AlertPage(WebDriver driver){
+    public AlertPage(WebDriver driver) {
         this.driver=driver;
     }
-
+    public void clickAlertAndAccept() {
+        driver.findElement(jsAlertButton).click();
+        Alert alert=driver.switchTo().alert();
+        alert.accept();
+    }
+    public void clickConfirmAndDismiss() {
+        driver.findElement(jsConfirmButton).click();
+        Alert alert=driver.switchTo().alert();
+        alert.dismiss();
+    }
+    public void clickPromptAndEnterText(String text) {
+        driver.findElement(jsPromptButton).click();
+        Alert alert=driver.switchTo().alert();
+        alert.sendKeys(text);
+        alert.accept();
+    }
+    public String getResultText() {
+        return driver.findElement(resultText).getText();
+    }
 }
